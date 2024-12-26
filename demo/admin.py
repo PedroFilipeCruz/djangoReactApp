22b35d567327c4c4610fe78c89a131d47e2a059f
Pedro Cruz <pedro.filipe.matos.cruz@gmail.com>
2024-12-26 18:52:39 +0000

    update

---
+++ demo/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price']
+    list_filter = ['published']
+    search_fields = ['title', 'description']
--- first/settings.py
+++ first/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    'demo',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = "first.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
